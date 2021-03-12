6752aee1322ffa08dcafce39c0c3f2779637b23e
Cleycione <80525359+Cleycione@users.noreply.github.com>
2021-03-12 10:37:11 -0300

    Treinamento
    cleycione

---
+++ Treinamento/Controllers/AlunoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Treinamento.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            string[] alunos = {
+                "Cleycione",
+                "João",
+                "José",
+                "Maria"
+            };
+
+            return View(alunos);
+        }
+    }
+}
